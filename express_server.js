18673c3e3f50f600205ea6364fe6a012c67da54b
gary92gs <gary92.gs@gmail.com>
2024-01-22 18:29:06 -0700

    learned about response.render and added a little bit of code about that

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+app.set('view engine', 'ejs');
+
 const urlDatabases = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -11,8 +13,12 @@ app.get('/', (request, response) => {
   response.send('Hello!');
 });
 
-app.get('/urls.json', (request, response) => { 
+app.get('/urls.json', (request, response) => {
   response.json(urlDatabases);
+});
+
+app.get('/hello', (request, response) => {
+  response.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
