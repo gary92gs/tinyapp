e79ad0a0a8a86e1967c94e94ec18d94ca8c600a1
gary92gs <gary92.gs@gmail.com>
2024-01-22 20:54:49 -0700

    changed up routes from most specific to least specific. debugged urls_show page, which had an extra ' ' character inside the header reference which was causing an error. Tested all pages, everything working

--- express_server.js
+++ express_server.js
@@ -9,25 +9,30 @@ const urlDatabases = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get('/', (request, response) => {
-  response.send('Hello!');
-});
-
 app.get('/hello', (request, response) => {
   const templateVars = { greeting: "Hello World!" };
   response.render("hello_world", templateVars);
+});
+
+//must be placed above get request for '/urls/:id'
+app.get('/urls/new', (request, response) => {
+  response.render('urls_new');
 });
 
 app.get('/urls', (request, response) => {
   const templateVars = { urls: urlDatabases };
   response.render('urls_index', templateVars);
 });
-
 app.get('/urls/:id', (request, response) => {
   const templateVars = { id: request.params.id, longURL: urlDatabases[request.params.id] };
   response.render('urls_show', templateVars);
 });
 
+app.get('/', (request, response) => {
+  response.send('Hello!');
+});
+
+//app.post();
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
