d4dc3a8221788854a182082eb280beca0a1908f0
gary92gs <gary92.gs@gmail.com>
2024-01-22 18:32:38 -0700

    install ejs, and made set it as the viewing engine for express server

--- express_server.js
+++ express_server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs'); //ejs needs to be installed to reference to it (note it is not 'required' above)
 
 const urlDatabases = {
   "b2xVn2": "http://www.lighthouselabs.ca",
