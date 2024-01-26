0a66e163a2b7bf8e17fd383ce63e1e9bf20b5a94
gary92gs <gary92.gs@gmail.com>
2024-01-25 19:47:49 -0700

    added cookies and chased down bugs

--- express_server.js
+++ express_server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
-const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
 const app = express();
 const PORT = 8080;
 
@@ -15,62 +15,49 @@ const urlDatabases = {
     longURL: "http://www.google.com",
     userID: "r2jZLU"
   },
-  "6tg5xK": {
+  "r2j5e3": {
     longURL: "http://www.google.com",
-    userID: "user2RandomID"
-  },
-  "42X5xK": {
-    longURL: "http://www.google.com",
-    userID: "userRandomID"
+    userID: "r2j5e3"
   },
 };
 
 const users = {
-  userRandomID: {
-    id: "userRandomID",
-    email: "a@a.com",
-    password: "done",
-  },
-  user2RandomID: {
-    id: "user2RandomID",
-    email: "b@b.com",
-    password: "doon",
-  },
   r2jZLU: {
     id: 'r2jZLU',
     email: 'gary92.gs@gmail.com',
-    password: bcrypt.hashSync('rocks', 10),
+    password: '$2a$10$7WoesiyZu1BhaTEZrtp28eJxdM7B5dbn2mAPPaKWWnORM9CO84K02', //amazing
   },
 };
 
 //MIDDLEWARE BELOW//
-
 app.use(express.urlencoded({ extended: true })); //this decodes the posted data before it reaches the server (ie. buffer => text)
-app.use(cookieParser());
+app.use(cookieSession({
+  name: 'session',
+  keys: ['catch22'],
+}));
 
 //ROUTES BELOW//
 
 //for READING Create New URL Page
 app.get('/urls/new', (request, response) => {
   //filter non-logged-in users(ie. no cookie or invalid cookie)
-  if (!request.cookies.uid || !Object.keys(users).includes(request.cookies.uid)) {
+  if (!request.session.uid || !Object.keys(users).includes(request.session.uid)) {
     return response.redirect('/login');
   }
   const templateVars = {
-    user: users[request.cookies.uid]
+    user: users[request.session.uid]
   };
 
-  response.render('urls_new', templateVars);
+  return response.render('urls_new', templateVars);
 });
 
 //for READING MyURLs page - listing all urls in database
 app.get('/urls', (request, response) => {
-  //console.log(Object.entries(urlDatabases))
   const templateVars = {
-    user: users[request.cookies.uid],
-    urls: getUserURLs(request.cookies.uid) //only pass the urls that belong to the current user
+    user: users[request.session.uid],
+    urls: getUserURLs(request.session.uid) //only pass the urls that belong to the current user
   };
-  response.render('urls_index', templateVars);
+  return response.render('urls_index', templateVars);
 });
 
 //for DELETING individual shortURLs
@@ -82,7 +69,7 @@ app.post('/urls/:id/delete', (request, response) => {
   }
   const urlID = urlDatabases[request.params.id].userID;
   //if cookie doesn't exist
-  const cookID = request.cookies.uid;
+  const cookID = request.session.uid;
   if (!cookID) {
     return response.status(401).send('Not Authorized. You must login');
   }
@@ -93,7 +80,7 @@ app.post('/urls/:id/delete', (request, response) => {
 
   const key = request.params.id;
   delete urlDatabases[key];
-  response.redirect('/urls'); //return page listing all urls
+  return response.redirect('/urls'); //return page listing all urls
 });
 
 //for UPDATING individual shortURLs
@@ -104,7 +91,7 @@ app.post('/urls/:id/update', (request, response) => {
     return response.status(404).send('Not Found. Shortened URL does not exist for the requested URL');
   }
   //if cookie doesn't exist
-  const cookID = request.cookies.uid;
+  const cookID = request.session.uid;
   if (!cookID) {
     return response.send('Not Authorized. You must login');
   }
@@ -117,7 +104,7 @@ app.post('/urls/:id/update', (request, response) => {
   const newURL = request.body.updatedURL;
   const key = request.params.id;
   urlDatabases[key].longURL = newURL;
-  response.redirect('/urls');
+  return response.redirect('/urls');
 });
 
 //for READING Inividual shortURLs
@@ -129,7 +116,7 @@ app.get('/urls/:id', (request, response) => {
   }
   const urlID = urlDatabases[request.params.id].userID;
   //if cookie doesn't exist
-  const cookID = request.cookies.uid;
+  const cookID = request.session.uid;
   if (!cookID) {
     return response.status(401).send('Not Authorized. You must login');
   }
@@ -139,47 +126,48 @@ app.get('/urls/:id', (request, response) => {
   }
 
   const templateVars = {
-    user: users[request.cookies.uid],
+    user: users[request.session.uid],
     id: request.params.id,
     longURL: urlDatabases[request.params.id].longURL,
   };
-  response.render('urls_show', templateVars);
+  
+  return response.render('urls_show', templateVars);
 });
 
 //for CREATING new links
 app.post('/urls', (request, response) => {
   //filter non-logged-in users (ie. no cookie or invalid cookie)
-  if (!request.cookies.uid || !Object.keys(users).includes(request.cookies.uid)) {
+  if (!request.session.uid || !Object.keys(users).includes(request.session.uid)) {
     return response.send('You must login to create short URLs\n');
   }
   const shortID = generateRandomString(request.body.longURL);
   urlDatabases[shortID] = {
     longURL: request.body.longURL,
-    userID: request.cookies.uid
+    userID: request.session.uid
   };
-  response.redirect(`/urls/${shortID}`); //then navigates to newly created link
+  return response.redirect(`/urls/${shortID}`); //then navigates to newly created link
 });
 
 //for READING original link of website from individual shortURL page
 app.get('/u/:id', (request, response) => {
-  //filter out requests for non-existent shortened urls
-  if (!Object.keys(urlDatabases).includes(request.params.id)) {
+  // filter out requests for non-existent shortened urls
+  if (!urlDatabases[request.params.id]) {
     return response.status(400).send('The short URL you have attempted to access does not exist\n');
   }
-  response.redirect(`${urlDatabases[request.params.id]}`);
+  return response.redirect(`${urlDatabases[request.params.id].longURL}`);
 });
 
 //for READING registration page
 app.get('/register', (request, response) => {
   //filter logged in users (ie. do they have a cookie)
-  if (Object.keys(users).includes(request.cookies.uid)) {
+  if (Object.keys(users).includes(request.session.uid)) {
     return response.redirect('/urls');
   }
   const templateVars = {
-    user: users[request.cookies.uid]
+    user: users[request.session.uid]
     // user: users['r2jZLU'] //for unlocking myself out of website
   };
-  response.render('register', templateVars);
+  return response.render('register', templateVars);
 });
 
 //for CREATING new user via registration page
@@ -204,19 +192,20 @@ app.post('/register', (request, response) => {
     password: hashedPassword,
   };
   //give user cookie containing ONLY the user id
-  response.cookie('uid', uid).redirect('/urls');
+  request.session.uid = uid;
+  return response.redirect('/urls');
 });
 
 //for lOGIN PAGE (READ)
 app.get('/login', (request, response) => {
   //filter logged in users (ie. do they have cookie with matching id)
-  if (Object.keys(users).includes(request.cookies.uid)) {
+  if (Object.keys(users).includes(request.session.uid)) {
     return response.redirect('/urls');
   }
   const templateVars = {
-    user: users[request.cookies.uid]
+    user: users[request.session.uid]
   };
-  response.render('login', templateVars);
+  return response.render('login', templateVars);
 });
 
 //for LOGGING IN (check user credentials)
@@ -229,19 +218,19 @@ app.post('/login', (request, response) => {
   } else {
     return response.status(403).send('Invalid Login Credentials');
   }
-
   //if user already registered and password correct, give cookie with uid
-  response.cookie('uid', `${user.uid}`).redirect('/urls');
-
+  request.session.uid = user.id;
+  return response.redirect('/urls');
 });
 
 //for LOGGING OUT (DELETE)
 app.post('/logout', (request, response) => {
-  response.clearCookie('uid').redirect('/login');
+  request.session = null;
+  return response.redirect('/login');
 });
 
 app.get('/', (request, response) => {
-  response.send('Hello!');
+  return response.send('Hello!');
 });
 
 
